ac11e779ab8d83d6bde7c965d179ca1eb5fb4757
Administrator <admin@module5.dockermk>
2017-03-02 21:31:54 +0300

    Update scripts

--- mk-helpers/seed_projects.sh
+++ mk-helpers/seed_projects.sh
@@ -3,13 +3,13 @@ source mk-helpers/env.vars
 eval $(docker-machine env $machine)
 
 
-BACKUP=1488464969_2017_03_02
-BACKUP_FILE=$BACKUP_gitlab_backup.tar.gz
+BACKUP=1488479292_2017_03_02
+BACKUP_FILE="$BACKUP"_gitlab_backup.tar
 
 printf "Подгтовка проектов\n"
 docker-compose exec gitlab wget https://s3.eu-central-1.amazonaws.com/docker-mk-mar-2017/module5/$BACKUP_FILE -P /var/opt/gitlab/backups/
 
-printf "Импортируем проекты\nОтвечаем 'yes' в терминале на вопросы\n"
+printf "Импортируем проекты\nОтвечаем 'yes' на вопросы из терминала\n"
 docker-compose exec gitlab /opt/gitlab/bin/gitlab-rake gitlab:backup:restore BACKUP=$BACKUP
 
 
@@ -26,3 +26,5 @@ done
 
 printf "\nРегистрируем CI агент\n"
 docker-compose exec gitlab_runner gitlab-runner register -n
+docker-compose exec gitlab_runner sed -i s/concurrent\ =\ 1/concurrent\ =\ 2/ /etc/gitlab-runner/config.toml
+docker-compose restart gitlab_runner
